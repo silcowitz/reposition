e887933744265302a8114123ac7f94b97a024c26
silcowitz <morten@silcowitz.dk>
2021-06-23 13:48:17 +0200

    latest, greatest, one hopes

--- eigentest.py
+++ eigentest.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.linalg import null_space
 
 np.set_printoptions(precision=3,linewidth=260)
 
@@ -7,11 +7,14 @@ M = np.array([[2,1,0,0],[1,2,1,0],[0,1,2,1],[0,0,1,2]])
 print(M)
 print(np.linalg.eigh(M))
 
+#A = np.random.rand(4,4)
+#M = A.dot(A.T)+np.eye(4)*0.01
+
 
 M2 = np.hstack( [M, np.zeros((4,1))])
 M2 = np.vstack( [M2, np.array([[0,0,0,0,1]])])
 
-print(M2)
+#print(M2)
 
 
 B = np.diag([1,1,1,1,-1])
@@ -22,44 +25,77 @@ print(np.linalg.eigh(B))
 R = np.vstack([ np.eye(5), np.array([[0,0,0,0,1]])])
 print(R)
 
-S = R.dot(np.linalg.inv(M2)).dot(R.T)
+print("svd")
+print(null_space(R.dot(R.T)))
 
+S = R.dot(np.linalg.inv(M2)).dot(R.T)
+print("eigh")
 print(np.linalg.eigh(S))
 Si = np.linalg.pinv(S)
+
+
+d,R2=np.linalg.eigh(S)
+t = 1.0*(d > 0)
+
+def ds(s):
+    u = d*(1-s) + t*s
+    return np.diag( [ (1.0/i if i>0 else 0.0) for i in u ])
+
+print(ds(1))
+
+#wefwafe
+#step_s = np.min(d[1:])/np.max(d[1:])
 
 #lawekjf
 z = np.random.randn(6,1)*1.0
 
 for t in range(int(9e9)):
-    z0 = z.copy()
-
-
+    s = (1.0/(t+1.0))
+    #print(s)
+    #Si = R2.dot(ds(np.random.rand(1))).dot(R2.T)
     g_z = 2*Si.dot(z)
-    z = z-(1.0/(t+1.0))*g_z
+    #s = 0.21
+    z = z-s*g_z
+    #z = S.dot(z)
 
+    z0 = z.copy()
     # project
-    p = np.linalg.norm(z[0:4])
-    u = np.abs(z[4])
-    lamb = (p-u)/(u+p)
-    if np.abs(lamb)>1:
-        lamb = 1.0/lamb
+    for j in range(138):
+        #project out of nullspace
+        #v = (z[4] + z[5])*.5
+        v = 1
+        z[4] = v
+        z[5] = v
 
-    #print(lamb)
-    if (lamb*lamb != 1):
-        z[0:4] *= 1.0/(1.0+lamb)
-        z[4] *= 1.0/(1.0-lamb)
+        p = np.linalg.norm(z[0:4])
+        u = np.abs(z[4])
+        lamb = (p-u)/(u+p)
+        if np.abs(lamb)>1:
+            lamb = 1.0/lamb
 
-    z[5] = 1
+        #print(lamb)
+        if (lamb*lamb != 1):
+            z[0:4] *= 1.0/(1.0+lamb)
+            z[4] *= 1.0/(1.0-lamb)
+
+        z[5] = 1
+
+        if (lamb*lamb < 1e-32):
+            #print('z[5] = %f' % z[5])
+            break
 
 
-    #project out of nullspace
-    v = (z[4] + z[5])*.5
-    z[4] = v
-    z[5] = v
-
+    #ljwoeif
 
     e = float((z-z0).T.dot(z-z0))
-    if e<1e-14 or t>2256:
+
+    n0 = g_z / np.linalg.norm(g_z)
+    n1 = (z-z0) / np.linalg.norm(z-z0)
+
+    e = 1.0-abs(n0.T.dot(n1))
+
+    print(e)
+    if e<1e-16 or t>1126:
         print('terminate at %d with error %.12f' % (t,e))
         break
 
@@ -80,4 +116,3 @@ v = s[0:4]
 print(np.linalg.norm(v))
 print(v/np.linalg.norm(v))
 #print(s / np.linalg.norm(v))
-
--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import scipy.linalg
 import os
 import sys
 import time
+from scipy.linalg import null_space
+from scipy.sparse.linalg import cg
 
-np.set_printoptions(precision=2,linewidth=260)
+np.set_printoptions(precision=2,linewidth=1260)
 
-def solve_newton( m, pi, maxiter ):
+def solve_newton( m, pi, lamb, maxiter ):
     P = len(m)
     L = P-1
 
@@ -29,7 +31,8 @@ def solve_newton( m, pi, maxiter ):
     p = np.zeros([P*3,1])
     p[:] = pi
     z = np.zeros([L*3,1])
-    lamb = np.zeros([L,1])
+    if lamb is None:
+        lamb = np.zeros([L,1])
     Q= np.zeros([L,L*3])
     D = np.zeros([L*3])
     Mi = np.zeros((P*3,P*3))
@@ -60,28 +63,93 @@ def solve_newton( m, pi, maxiter ):
         #lamb *=0
 
         D[:] = 0
+        lamb_sum = 0
         for j in range(L):
             zi = z[j*3:(j+1)*3]
 
             f = np.linalg.norm(zi)*np.sqrt(2)
+            #f = 1.0
             z[j*3:(j+1)*3] /= f
-            lamb[j] *= f
+            lamb[j] /= f
+            lamb_sum += lamb[j]
+            Q[j,j*3:(j+1)*3] = z[j*3:(j+1)*3].T * 1
+            D[ j*3:(j+1)*3] = lamb[j] if lamb[j] > 0 else 0
 
-            Q[j,j*3:(j+1)*3] = z[j*3:(j+1)*3].T
-            D[ j*3:(j+1)*3] = lamb[j]
+
+        if False:
+            for j in range(L):
+                zi = z[j*3:(j+1)*3]
+                D[ j*3:(j+1)*3] = 1*lamb_sum / L
+
+
 
         b_z = Si.dot(z-R.dot(p)) + Q.T.dot(lamb)
-        b_lamb = 0.5*(np.diag(Q.dot(Q.T)).reshape(-1,1) - np.ones((L,1))*.5)
+        b_lamb = 0.5*(np.diag(Q.dot(Q.T)).reshape(-1,1) - np.ones((L,1))*.5) * 0.0
 
         #print("b vectors")
         #print(b_z)
-        #print(b_lamb)
+        #print(Si.dot(N))
+
+        D2 = np.diag(np.ones_like(D))*(lamb_sum/L)
+        #print(D2)
+        #print(np.diag(D))
+        Sp = np.linalg.inv(Si+np.diag(D)*1+D2*0)
+        #Spdd = np.linalg.inv(Si+np.diag(D)*0+D2*1)
+
+
+        Spdd = np.linalg.inv(np.diag(np.diag(Si))+np.diag(D))
+        QQp = np.linalg.inv(Q.dot(Sp).dot(Q.T))
+
+        #print("null space")
+        #print(Q)
+        #print(null_space(Q))
+        #print(null_space(Q).shape)
+        #N = null_space(Q)
+        #print(N.T.dot(Si+np.diag(D)).dot(N))
+        #print(np.linalg.cond(Si+np.diag(D)))
+        #print(np.linalg.cond(N.T.dot(Si+np.diag(D)).dot(N)))
+        #QQp = np.eye(L)
+        #Sp = Spd
+        #print( Sp )
+        #print("reduced system, lambda=0")
+        #print( Q.dot(R.dot(R.T)).dot(Q.T) )
+        #QQp = np.linalg.inv(Q.dot(Q.T))
+        #Sp = np.eye(L*3)
+        #print(np.linalg.cond(P.dot(Si+np.diag(D))))
 
         #Si = RR
-        A = np.vstack( [ np.hstack([ Si +np.diag(D), Q.T]), np.hstack([Q, np.zeros((L,L))]) ])
-        #print(A)
+        A = np.vstack( [ np.hstack([ Si+np.diag(D), Q.T]), np.hstack([Q, np.zeros((L,L))]) ])
+        P = np.vstack( [ np.hstack( [ Sp-Sp.dot(Q.T).dot(QQp).dot(Q).dot(Sp), Sp.dot(Q.T).dot(QQp) ]), np.hstack( [ QQp.dot(Q).dot(Sp), -QQp ]) ] )
+
+        #print("condition number")
+        #print(np.linalg.cond(P.dot(A)))
+        #print("complement condition number")
+        #print(np.linalg.cond(    np.linalg.inv(Q.dot(Spdd).dot(Q.T).dot(   Q.dot(Sp).dot(Q.T) )    )))
+
+        #print(np.linalg.inv(Si+np.diag(D)))
+        #print(np.diag(P.dot(A)))
+        #print(np.linalg.cond(Q.dot(Sp).dot(Q.T)))
+
+
+
         b = np.vstack( [ b_z, b_lamb ])
-        s = np.linalg.pinv(A).dot(b)
+
+        #s = np.linalg.pinv(A).dot(b)
+
+        if True:
+            qq,it = cg( Q.dot(Sp).dot(Q.T), Q.dot(Sp.dot(b_z))-b_lamb, M=np.linalg.inv(Q.dot(Spdd).dot(Q.T)), tol=1e-14, maxiter=15,callback = lambda x: print('iter2,',end='') )
+            #s,it = cg( A, b, tol=1e-14, maxiter=15,callback = lambda x: print('iter') )
+        #print("qq=")
+        #print(qq)
+
+        if True:
+            s,it = cg( A, b, tol=1e-14, maxiter=15, M = P,callback = lambda x: print('iter,',end='') )
+            #s,it = cg( A, b, tol=1e-14, maxiter=15,callback = lambda x: print('iter') )
+            s = s.reshape(-1,1)
+
+        #s = P.dot(b)
+
+        #print(np.linalg.inv(A))
         #print(s)
 
         #print(Q)
@@ -98,7 +166,7 @@ def solve_newton( m, pi, maxiter ):
         #print(dlamb)
         #print(s)
         #liajwoeijf
-        scale = np.linalg.norm(s)
+        scale = np.linalg.norm(s) + 0.0001
         s /= scale
 
         dz = s[0:L*3]
@@ -106,7 +174,7 @@ def solve_newton( m, pi, maxiter ):
 
         step = min(scale,scale)
         z -= dz * step
-        lamb -= dlamb*step
+        lamb -= dlamb * step
 
 
         e = np.linalg.norm(b)**2
@@ -119,7 +187,7 @@ def solve_newton( m, pi, maxiter ):
             #print(lamb)
             s = Si.dot(R.dot(p)-z)
             x = p-Mi.dot(R.T.dot(s))
-            return x
+            return x, lamb
 
 def solve_pgd( m, pi, maxiter, pre_z = None):
     P = len(m)
@@ -157,14 +225,15 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
     Mip = np.zeros((P*3,P*3))
 
     z = R.dot(p)
-    for i in range(L):
-        zi = z[i*3:(i+1)*3,0]
-        znorm = np.linalg.norm(zi)
-        zi /= znorm * np.sqrt(2)
+    if False:
+        for i in range(L):
+            zi = z[i*3:(i+1)*3,0]
+            znorm = np.linalg.norm(zi)
+            zi /= znorm * np.sqrt(2)
 
     #z = np.random.randn(L*3,1)*0
 
-    if not pre_z is None:
+    if not pre_z is None and True:
         z = pre_z
 
     for i in range(P):
@@ -175,21 +244,28 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
 
     RR = R.dot(Mi.dot(R.T))
 
-    d,r = np.linalg.eigh(RR)
+    C = np.linalg.cholesky(RR)
+    Ci = np.linalg.inv(C)
+    q = Ci.dot(z)
+    #print(q)
+    #print(RR-C.dot(C.T))
+    #wef
+    #lkwjef
+    #wer
+
+    Du,U = np.linalg.eigh(RR)
     #print(np.max(d))
     #print(np.min(d))
-    print(d)
+    print(Du)
     #d_mask[0:3] = 0.0
-    #d[32:] = 0.0
-    d_mask = np.array([ 1.0/e if e > 0 else 0.0 for e in d])
-
-
-    #d_mask[:] = 1.0
-    print(d_mask)
+    #d[453:] = 0.0
+    d_mask = np.array([ 1.0/e if 1.0/e > 0 else 0.0 for e in Du])
     d_scale = np.linalg.norm(d_mask)
-    #d_scale = 1.0
-    print(d_mask)
-    RRip = r.dot(np.diag(d_mask)).dot(r.T) * (1.0/d_scale)
+    d_scale = np.max(d_mask)
+    d_scale = 1.0
+    print("d_scale")
+    print(d_scale)
+    RRip = U.dot(np.diag(d_mask)).dot(U.T) * (1.0/d_scale)
 
 
 
@@ -201,15 +277,25 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
     #print(rank)
 
     RRi = scipy.linalg.pinvh(RR)
+    RRn = scipy.linalg.pinvh(R.dot(R.T))
     c = RRip.dot(R.dot(p))
-    print(np.linalg.norm( R.T.dot(RRi).dot(r), axis=0) )
-    print(r[1])
+    cq = Ci.dot(R.dot(p))
+    #print(np.linalg.norm( R.T.dot(RRi).dot(r), axis=0) )
+    #print(r[1])
     #wfwe
     #print(z)
+    e = 1
+
+
+    w = U.T.dot(z)
+
+    B = np.hstack( [ R, -np.eye((L)*3)])
+    print(B)
+    x = p.copy()
     for i in range(maxiter):
         #print("inner iter %d" % i )
         z0 = z.copy()
-
+        w0 = w.copy()
         #RRi(z+u) = RRiRtp
         # solve for update
         #y = np.linalg.solve(RR+np.eye(L*3)*3, z-R.dot(p))
@@ -217,7 +303,13 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
         #S = (RR)
         #y = S.dot(z)-c
 
-        g_z =  2*RRip.dot(z) - 2*RRip.dot(R.dot(p)) + np.random.randn(L*3,1) * 0.0
+        g_z =  2*RRip.dot(z) - 2*RRip.dot(R.dot(p))
+        g_w = 2*Du.dot(w) - 2*Du.dot(U.dot(R).dot(p))
+
+
+        #g_q = (2*q - 2*cq)
+        #print(g_q)
+
         #g_z = 2*RR.dot(z)-2*R.dot(p)
         #g_z =  .5*RR.dot(g)
 
@@ -230,7 +322,32 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
         #dznorm = np.linalg.norm(dz)
         #print("dz norm")
         #print(dznorm )
-        y = z - (2/(i*1+2))*g_z * d_scale
+
+        if g_z.T.dot(g_z) > 0:
+            step = float( g_z.T.dot(z-R.dot(p)) / (g_z.T.dot(RRip).dot(g_z)) )
+        else:
+            step = 1
+
+        #print(step)
+        step = (1.0/(i+1))
+        #step = step if step < 0.004 else 0.004
+
+
+
+        step = 0.2
+        #z = z*1.0 - step*g_z * d_scale
+
+        #z = z + t*0.1
+
+        w = w - step*g_w
+
+        #qs = q - (2/(i*1+2))*g_q
+        #y = z
+
+        #print("g_q norm")
+        #print(np.linalg.norm(g_q))
+        #y = z - (2/(i*1+2))* ( g_z.T.dot(g_z) / g_z.T.dot(RRip.dot(g_z)) ) * g_z
+
         #gznorm =  np.linalg.norm(g_z)
         #y = z -(g_z / gznorm) * (2/(i+2))* min(5.1,gznorm)
 
@@ -254,32 +371,45 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
         #y = z*.5 + y*.5
         #y = z - u
         # normalize z
-        D[:] = 0
-        for j in range(L):
-            yi = y[j*3:(j+1)*3]
-            zi = z[j*3:(j+1)*3]
-            sign = 1.0 if zi.T.dot(yi) >= 0 else -1.0
-            ynorm = np.linalg.norm(yi)
-            #print(ynorm)
-            sign = 1
-            if (sign<0):
-                print("sign change")
+        #D[:] = 0
 
-            if ynorm > 0.5*np.sqrt(2) or True:
+        # project
+        #z = U.dot(w)
 
-                #v = yi *.5 + zi*.5
-                z[j*3:(j+1)*3] =  yi  * sign * (1.0/(ynorm)) * (1.0/np.sqrt(2))
-                #lamb[j] *= sign * ynorm*np.sqrt(2)
-                #lamb[j] = sign*(ynorm*np.sqrt(2)-1.0)
+        x = x - 1.0*(2*x-2*p)
 
+        for k in range(145):
+            #print(z)
+            for j in range(L):
+                zi = z[j*3:(j+1)*3]
+                ynorm = np.linalg.norm(zi) * 1.0 #* (1-(1/(i+1)))
+                if True: #0.5*np.sqrt(2):
+                    #print('projecting '+str(ynorm))
+                    z[j*3:(j+1)*3] =  zi  * (1.0/(ynorm)) * (1.0/np.sqrt(2))
 
-            else:
-                z[j*3:(j+1)*3] = yi
-                pass
-                #lamb[j] = 0.0
-
-            Q[j,j*3:(j+1)*3] = z[j*3:(j+1)*3].T
+                Q[j,j*3:(j+1)*3] = z[j*3:(j+1)*3].T
             #D[ j*3:(j+1)*3] = lamb[j]
+
+
+
+            v = np.vstack( [ x, z])
+            P = np.eye((L+(L+1))*3)-B.T.dot(np.linalg.inv(B.dot(B.T))).dot(B)
+            v2 = P.dot(v)
+            x = v2[:(L+1)*3]
+            z = v2[(L+1)*3:]
+
+        for k in range(1):
+            #print(z)
+            for j in range(L):
+                zi = z[j*3:(j+1)*3]
+                ynorm = np.linalg.norm(zi) * 1.0 #* (1-(1/(i+1)))
+                if True: #0.5*np.sqrt(2):
+                    #print('projecting '+str(ynorm))
+                    z[j*3:(j+1)*3] =  zi  * (1.0/(ynorm)) * (1.0/np.sqrt(2))
+
+                Q[j,j*3:(j+1)*3] = z[j*3:(j+1)*3].T
+        #print(p)
+        #w = U.T.dot(z)
 
         #print(np.linalg.pinv(Q.T))
         #print(Q.dot(RR.dot(Q.T)))
@@ -295,8 +425,11 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
         #lamb -= d_lamb
         #lamb *= lamb > 0
 
-        c2 = RRip.dot(z)-c
-        lamb =  -np.linalg.inv(Q.dot(Q.T)).dot(Q).dot(c2) * 1
+        # calc minimal error given z
+        if True:
+            c2 = RRip.dot(z)-c
+            lamb =  -np.linalg.inv(Q.dot(Q.T)).dot(Q).dot(c2) * 1
+            g_z = 2*RRip.dot(z) - 2*c + 2*Q.T.dot(lamb)
 
 
 
@@ -310,26 +443,30 @@ def solve_pgd( m, pi, maxiter, pre_z = None):
 
         #lkwjef
         #print("g norm after lambda")
-        g_z = 2*RRip.dot(z) - 2*c + 2*Q.T.dot(lamb)
         #znorm = np.linalg.norm(z)
         #e = (z.T.dot(-g_z)/(gznorm*znorm))
-        e = np.linalg.norm(g_z)
-        #e = np.linalg.norm(z-z0)
+        #e = np.linalg.norm(g_z)
+        e = np.linalg.norm(z-z0)
+        #e = np.linalg.norm(w-w0)
+        #e = np.linalg.norm(q-q0)
         if False:
             print(e)
         else:
             print(e, end='' )
             print('         ',end='')
             print('\r', end='')
-        if (e < 1e-20 or i==maxiter-1 and i!=0 ):
+        if (e < 1e-5 or i==maxiter-1 and i!=0 ):
 
             print("solve_pgd converged at iter %d with" % i)
             print(e)
             #print(z)
             #print(lamb)
             #s = np.linalg.solve(RR, R.dot(p)-z)
-            s = np.linalg.pinv(RR).dot(R.dot(p)-z)
-            x = p-Mi.dot(R.T.dot(s))
+            #z = C.dot(q)
+            #print(zq)
+            #z = U.dot(w)
+            s = RRi.dot(R.dot(p)-z)
+            #x = p-Mi.dot(R.T.dot(s))
             return x,z,lamb
 
 #    if False:
@@ -413,7 +550,8 @@ def solve_power( m, pi, maxiter, pre_z = None, pre_lamb = None ):
         #y = S.dot(z)-c
 
         g_z = 2*RRi.dot(z) - 2*c + 2*Q.T.dot(lamb)
-        dz = np.linalg.solve( 2*(RRi+np.diag(D)), -g_z)
+        #dz = np.linalg.solve( 2*(RRi+np.diag(D)), -g_z)
+        dz = .5*RR.dot( np.linalg.solve( np.eye(L*3)+np.diag(D).dot(RR), -g_z) )
         #dz = (.5*RR+np.diag(D)).dot(-g_z)
         dznorm = np.linalg.norm(dz)
         #print("dz norm")
@@ -449,11 +587,11 @@ def solve_power( m, pi, maxiter, pre_z = None, pre_lamb = None ):
             if (sign<0):
                 print("sign change")
 
-            if ynorm > 1e-17 and True:
+            if ynorm >= np.sqrt(2)/2 or True:
                 #v = yi *.5 + zi*.5
                 z[j*3:(j+1)*3] =  yi  * sign * (1.0/(ynorm)) * (1.0/np.sqrt(2))
                 lamb[j] *= sign * ynorm*np.sqrt(2)
-                lamb[j] = sign*(ynorm*np.sqrt(2)-1.0)
+                #lamb[j] = sign*(ynorm*np.sqrt(2)-1.0)
 
 
             else:
@@ -470,10 +608,12 @@ def solve_power( m, pi, maxiter, pre_z = None, pre_lamb = None ):
 
         #Si = np.linalg.inv(RRi+np.diag(D))
         SS = RR
+        #SS = np.diag(np.diag(RR))
         g_z = RRi.dot(z) - c + Q.T.dot(lamb)
         g_lamb = .5*(np.diag(Q.dot(Q.T)).reshape(-1,1) - np.ones((L,1))*.5)
         #print(g_lamb)
         d_lamb = np.linalg.solve( Q.dot(SS).dot(Q.T), Q.dot(SS).dot(g_z) - g_lamb * 0)
+        #print(Q.dot(SS).dot(Q.T))
         #print(d_lamb)
         lamb -= d_lamb
         #lamb *= lamb > 0
@@ -518,32 +658,33 @@ def solve_power( m, pi, maxiter, pre_z = None, pre_lamb = None ):
 
 
 
-import sys, pygame
-pygame.init()
+if False:
+    import sys, pygame
+    pygame.init()
 
-size = width, height = 2*320, 2*240
-speed = [2, 2]
-black = 0, 0, 0
+    size = width, height = 2*320, 2*240
+    speed = [2, 2]
+    black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(size)
 
 
 
-N = 32
+N = 64
 p = np.zeros((N*3,1))
 m = np.zeros((N,1))
 p0 = np.array([[0.0,0.0,0.0]])
 zigzag = np.array([[1,0,0]])
 for i in range(N):
     p[i*3:(i+1)*3,0] = p0
-    noise = 0.01
+    noise = 0.0
     delta2 = np.random.randn(1,3) * noise
     zigzag*= -1
-    delta = np.array([[0,1,0]],dtype=np.float32) + zigzag + noise
+    delta = np.array([[0,1,0]],dtype=np.float32) + zigzag + delta2
     delta /= np.linalg.norm(delta)
     f = 1.0 if i<N-1 else 1.0
     p0 += delta * f
-    m[i] = 1.0 if i==N-1 or i==0 or i==N/2  else 1.0
+    m[i] = 10000.0 if  i==N-1   else 1.0
 
 
 
@@ -562,8 +703,8 @@ for i in range(0):
 
     print("target p=")
     print(p0)
-    r_power = solve_power( m, p0, 125 )
-    r_newton = solve_newton( m, p0, 145 )
+    r_power = solve_power( m, p0, 1125 )
+    r_newton = solve_newton( m, p0, None, 145 )
 
     print("result=")
     print(np.array( [r_power-r_newton, r_power, r_newton ]).T)
@@ -574,43 +715,47 @@ x0 = p.copy()
 x1 = p.copy()
 z = None
 lamb = None
+lamb_newton = None
 for i in range(2115):
 
+    dt = 0.1
 
-
-    v = x1-x0
+    v = (x1-x0)/dt
     #print("velocity")
     #print(v)
     x0[:] = x1
-    x1 += v
+    x1 += v*dt
 
     for j in range(N):
-        x1[j*3:(j+1)*3,0] += np.array([1,0,0]) * 0.098
+        x1[j*3:(j+1)*3,0] += np.array([1,0,0]) * 0.098 * dt
     #print("targets")
     #print(x1)
     x1[0:3] *=0 # pin
-    xp,z,lamb = solve_pgd(m, x1, 2000, pre_z = z)
+    xp,z,lamb = solve_power(m, x1, 32, pre_z = z, pre_lamb= lamb )
 
-    y = solve_newton(m, x1, 120 )
+
+
+    y,lamb_newton = solve_newton(m, x1, lamb_newton, 32 )
     l = np.linalg.norm(xp-y)
     print("solution divergence")
     print(l)
-    if l>0.1 or True:
+    if l>0.1 and False:
         print("x values")
         print( xp)
         #print(lamb)
         #print(xp-y)
     x1[:] = xp
 
-    for i in range(1,N):
-        pp0 = 15*xp[(i-1)*3:i*3].flatten() + [55,55,55]
-        pp1 = 15*xp[i*3:(i+1)*3].flatten() + [55,55,55]
-        print(pp0)
-        print(pp1)
-        pygame.draw.line(screen, (255,255,255), pp0[:2][::-1], pp1[:2][::-1], 2)
+    if False:
+        for i in range(1,N):
+            pp0 = 15*xp[(i-1)*3:i*3].flatten() + [55,55,55]
+            pp1 = 15*xp[i*3:(i+1)*3].flatten() + [55,55,55]
+            #print(pp0)
+            #print(pp1)
+            pygame.draw.line(screen, (255,255,255), pp0[:2][::-1], pp1[:2][::-1], 2)
 
-    pygame.display.update()
-    screen.fill((0,0,0))
+        pygame.display.update()
+        screen.fill((0,0,0))
     #print("position")
     #print(x1)
 
