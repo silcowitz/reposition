b0ba412ffb5ab1eb972d4de97a8de6b97d2176c2
Morten Silcowitz <morten@silcowitz.dk>
2026-02-18 11:33:38 +0100

    prettier

--- reposition/src/main.js
+++ reposition/src/main.js
@@ -1,58 +1,64 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-import { useSolver } from '@root/reposition.js'
-
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import { useSolver } from "@root/reposition.js";
 
 const solver = await useSolver(1024);
 
-
 const N = 1024;
-const p = new Float64Array(N*3);
-const x = new Float64Array(N*3);
-const x0 = new Float64Array(N*3);
+const p = new Float64Array(N * 3);
+const x = new Float64Array(N * 3);
+const x0 = new Float64Array(N * 3);
 const m = new Float64Array(N);
-for (let i=0; i<N; i++ ) {
- x[i*3+0] = i * Math.cos(i * 0.1) * 0.1;
- x[i*3+1] = i * Math.sin(i * 0.1) * 0.1;
- x[i*3+2] = 0;
- m[i] = (i=== 0 || i===N-1)  ? 1 : 0.000001;
+for (let i = 0; i < N; i++) {
+    x[i * 3 + 0] = i * Math.cos(i * 0.1) * 0.1;
+    x[i * 3 + 1] = i * Math.sin(i * 0.1) * 0.1;
+    x[i * 3 + 2] = 0;
+    m[i] = i === 0 || i === N - 1 ? 1 : 0.000001;
 }
 x0.set(x);
 p.set(x);
 
 //console.log(x);
 import * as THREE from "three";
-import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
-import { Line2 } from 'three/addons/lines/Line2.js';
-import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
+import { LineMaterial } from "three/addons/lines/LineMaterial.js";
+import { Line2 } from "three/addons/lines/Line2.js";
+import { LineGeometry } from "three/addons/lines/LineGeometry.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 const renderer = new THREE.WebGLRenderer();
 document.body.appendChild(renderer.domElement);
 
 const geometry = new LineGeometry();
-const vertices = new Float32Array(N*3);
+const vertices = new Float32Array(N * 3);
 geometry.setPositions(vertices);
 
-const material = new LineMaterial({ color: 0xffff00, linewidth: 2, vertexColors: true });
+const material = new LineMaterial({
+    color: 0xffff00,
+    linewidth: 2,
+    vertexColors: true,
+});
 const line = new Line2(geometry, material);
 
 const colors = new Float32Array(N * 3);
 
 for (let i = 0; i < N; i++) {
-  if (i % 2 === 0) {
-    colors[i*3+0] = 1;
-    colors[i*3+1] = 1;
-    colors[i*3+2] = 0;
-  } else {
-    colors[i*3+0] = 0;
-    colors[i*3+1] = 0;
-    colors[i*3+2] = 1;
-  }
+    if (i % 2 === 0) {
+        colors[i * 3 + 0] = 1;
+        colors[i * 3 + 1] = 1;
+        colors[i * 3 + 2] = 0;
+    } else {
+        colors[i * 3 + 0] = 0;
+        colors[i * 3 + 1] = 0;
+        colors[i * 3 + 2] = 1;
+    }
 }
 
 geometry.setColors(colors);
@@ -60,7 +66,7 @@ geometry.setColors(colors);
 material.resolution.set(window.innerWidth, window.innerHeight);
 line.computeLineDistances();
 scene.add(line);
-camera.position.z = 264;
+camera.position.z = 364;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x404040);
@@ -70,15 +76,15 @@ function animate() {
     requestAnimationFrame(animate);
 
     for (let i = 0; i < p.length; i++) {
-        p[i] = x[i] + (x[i] - x0[i])*0.99;
+        p[i] = x[i] + (x[i] - x0[i]) * 0.99;
     }
 
-    x0.set(x)
+    x0.set(x);
     if (once) {
-        solver.solve(m.length,p,m,x);
+        solver.solve(m.length, p, m, x);
         once = true;
     }
-    vertices.set(x)
+    vertices.set(x);
     //geometry.attributes.position.needsUpdate = true;
     geometry.setPositions(vertices);
 
@@ -106,5 +112,3 @@ animate();
 //`
 
 //setupCounter(document.querySelector('#counter'))
-
-
